1acb450be8e87ec076075ddddc74347109d577d6
yunderboy <emil.konstantinovitz@gmail.com>
2016-01-21 15:03:43 +0100

    added HTTPS, added editMode to user profiles

---
+++ client/assets/js/app.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations',
+
+    // custom
+    'controllers',
+    'ngResource',
+    'services',
+    'resources',
+    'filters',
+    'ng-token-auth'
+  ])
+
+
+  // Setup authenticantion
+  .config(function($authProvider) {
+    $authProvider.configure({
+        apiUrl: 'https://maecen-staging.herokuapp.com/v1',
+        //apiUrl: 'http://localhost:3000/v1',
+        storage: 'localStorage'
+    });
+  })
+  // What's this?...
+    .config(config)
+    .run(run);
+
+  // Setup routing
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+})();
---
+++ client/assets/js/resources.js
@@ -0,0 +1,15 @@
+(function(){
+  var baseUrl = "https://maecen-staging.herokuapp.com/"
+  //var baseUrl = "http://localhost:3000/"
+  var urlVersion = "v1/"
+  angular.module("resources", ["ngResource"])
+  // Users
+  .factory("User", function($resource) {
+    return $resource(baseUrl+urlVersion+"users/:id", { id: "@id"});
+  })
+  // Projects
+  .factory("Project", function($resource) {
+    return $resource(baseUrl+urlVersion+"projects/:id");
+  })
+
+})();
